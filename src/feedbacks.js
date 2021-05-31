3efd670398251dd6bbc7c5a5924c83c5dc19b138
sergiojc90 <sergiojc90@gmail.com>
2021-05-31 16:53:12 -0500

    Creating feedbacks module

---
+++ src/feedbacks.js
@@ -0,0 +1,42 @@
+const menuSection = (contentDiv) => {
+
+    const feedbackItems = [
+        {name:"Sergio Juárez",position:"Food Specialist",comment:"Amazing food, thanks for the experience"},
+        {name:"Emma Rosado",position:"Food Specialist",comment:"The food is unbelievable"},
+        {name:"Wendy Vázquez",position:"Food Specialist",comment:"I can't wait to come back for more"},
+    ]
+
+    const section = document.createElement("section");
+    const h1 = document.createElement("h1");
+    const feedbacks = document.createElement("article");
+
+    feedbackItems.forEach(element => {
+        const feedbackItem = document.createElement("div");
+        const feedbackName = document.createElement("h2");
+        const feedbackPosition = document.createElement("p");
+        const feedbackComment = document.createElement("p");
+
+        feedbackItem.classList.add("content--feedback--item");
+        feedbackName.classList.add("content--feedback--item__name");
+        feedbackPosition.classList.add("content--feedback--item__position");
+        feedbackComment.classList.add("content--feedback--item__comment");
+
+        feedbackName.textContent = element.name;
+        feedbackPosition.textContent = element.position;
+        feedbackComment.textContent = element.comment;
+
+        feedbackItem.appendChild(feedbackName);
+        feedbackItem.appendChild(feedbackPosition);
+        feedbackItem.appendChild(feedbackComment);
+
+        feedbacks.appendChild(feedbackItem);
+    });
+
+    
+    section.appendChild(h1);
+    section.appendChild(feedbacks);
+    
+    contentDiv.appendChild(section);
+}
+
+export {menuSection};
