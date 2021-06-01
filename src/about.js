3e31af4b0860bfbe7400a3a284c59d9477ef3ce3
sergiojc90 <sergiojc90@gmail.com>
2021-06-01 17:59:21 -0500

    Eliminating aditional buttons

--- src/about.js
+++ src/about.js
@@ -8,7 +8,6 @@ const aboutSection = (contentDiv) => {
     const description = document.createElement("div");
     const h2 = document.createElement("h2");
     const p = document.createElement("p");
-    const button = document.createElement("button");
     const subImage = document.createElement("img");
 
     section.classList.add("content--about--section");
@@ -19,18 +18,15 @@ const aboutSection = (contentDiv) => {
     description.classList.add("content--about__description");
     h2.classList.add("content--about__h2");
     p.classList.add("content--about__p");
-    button.classList.add("content--about__button");
     subImage.classList.add("content--about__subImage");
 
     h1.textContent = "About Us";
 
     h2.textContent = "Welcome to Dine Out Restaurant"
     p.textContent = "Dine Out is well known with a substantial gathering of people including families, kids, seniors and business experts. Our benevolent condition is perfect for praising unique events, facilitating a business lunch or assembling for a flavorful dinner with loved ones. ";
-    button.textContent = "Check Our Menu";
 
     description.appendChild(h2);
     description.appendChild(p);
-    description.appendChild(button);
 
     subSection.appendChild(description);
     subSection.appendChild(subImage);
