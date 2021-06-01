a9a801ab4e916b5ccb1205f8abca9a00867a635b
sergiojc90 <sergiojc90@gmail.com>
2021-06-01 00:08:55 -0500

    changing styles and adding a toggle button for responsive design

--- src/nav.js
+++ src/nav.js
@@ -5,8 +5,14 @@ const navBar = (contentDiv,sections) =>{
     const span = document.createElement("span");
     const aLogo = document.createElement("a");
     const ul = document.createElement("ul");
+    const toggleButton = document.createElement("a");
+
+    toggleButton.appendChild(document.createElement("span"));
+    toggleButton.appendChild(document.createElement("span"));
+    toggleButton.appendChild(document.createElement("span"));
 
     nav.classList.add("nav");
+    toggleButton.classList.add("nav--toggle-button");
     logo.classList.add("nav--logo");
     aLogo.classList.add("nav--logo__a");
     aLogo.textContent = "Dine Out";
@@ -20,15 +26,21 @@ const navBar = (contentDiv,sections) =>{
     sections.forEach((element) => {
         const li = document.createElement("li");
         const a = document.createElement("a");
-        a.classList.add("nav--ul__a")
+        li.classList.add("nav--ul__li");
+        li.id = element
         a.textContent = element;
-        a.id = element;
         li.appendChild(a);
         ul.appendChild(li);
     });
 
     nav.appendChild(logo)
     nav.appendChild(ul);
+    nav.appendChild(toggleButton);
+
+    toggleButton.addEventListener("click",() =>{
+        const navBarLinks = document.querySelector(".nav--ul");
+        navBarLinks.classList.toggle("active")
+    })
 
     contentDiv.appendChild(nav);
 };
