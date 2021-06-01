480ea4df7d7669c63fee4e261a2f49474ed95f09
sergiojc90 <sergiojc90@gmail.com>
2021-05-31 22:39:58 -0500

    creating basic css and adding font links to html

--- src/nav.js
+++ src/nav.js
@@ -2,11 +2,20 @@ const navBar = (contentDiv,sections) =>{
 
     const nav = document.createElement("nav");
     const logo = document.createElement("div");
+    const span = document.createElement("span");
+    const aLogo = document.createElement("a");
     const ul = document.createElement("ul");
 
     nav.classList.add("nav");
     logo.classList.add("nav--logo");
+    aLogo.classList.add("nav--logo__a");
+    aLogo.textContent = "Dine Out";
+    span.classList.add("material-icons");
+    span.textContent = "restaurant";
     ul.classList.add("nav--ul");
+
+    aLogo.appendChild(span);
+    logo.appendChild(aLogo);
     
     sections.forEach((element) => {
         const li = document.createElement("li");
